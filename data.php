b8b4457df00b1e2ff3fd7c332cc2cacaccde5a03
Dean Verhey <deanverhey@gmail.com>
2017-02-26 20:33:28 -0800

    capture user form inputs

--- data.php
+++ data.php
@@ -12,7 +12,31 @@
 <?php include('include/header.php')?>
 
 <!--PAGE CONTENT-->
+<?php
+    //traffic=high & housing=high & walkability=high & nonviolentcrime=high &rent=high &violentcrime=high
+    //Capture vars from query string
+    $traffic = $_GET['traffic'];
+    $housing = $_GET['housing'];
+    $walk = $_GET['walkability'];
+    $nvCrime = $_GET['nonviolentcrime'];
+    $rent = $_GET['rent'];
+    $violentCrime = $_GET['violentcrime'];
+?>
 
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <?php
+                echo "Traffic: $traffic <br>";
+                echo "Housing: $housing <br>";
+                echo "Walk: $walk <br>";
+                echo "Nonviolent Crime: $nvCrime <br>";
+                echo "Rent: $rent <br>";
+                echo "Violent Crime: $violentCrime <br>"
+            ?>
+        </div>
+    </div>
+</div>
 
 <!--ABOUT CARDS-->
 <?php include('include/aboutcards.php')?>
