268f69a664d606ebed3f8ebdb043b4908a64eb77
Dean Verhey <deanverhey@gmail.com>
2017-03-03 23:30:49 -0800

    set up db connections, get data for visualizations

--- data.php
+++ data.php
@@ -30,6 +30,14 @@
     $nvCrime = $_GET['nonviolentcrime'];
     $rent = $_GET['rent'];
     $violentCrime = $_GET['violentcrime'];
+
+    //Store values in cookie
+    setcookie("SL_Options_Traffic", $traffic);
+    setcookie("SL_Options_Housing", $housing);
+    setcookie("SL_Options_Walk", $walk);
+    setcookie("SL_Options_NVCrime", $nvCrime);
+    setcookie("SL_Options_Rent", $rent);
+    setcookie("SL_Options_VCrime", $violentCrime);
 ?>
 
 <!--PAGE CONTENT-->
@@ -37,29 +45,50 @@
     <div class="row">
         <div class="col-12">
             <?php
-                //Debug - show user input
-                echo "Traffic: $traffic <br>";
-                echo "Housing: $housing <br>";
-                echo "Walk: $walk <br>";
-                echo "Nonviolent Crime: $nvCrime <br>";
-                echo "Rent: $rent <br>";
-                echo "Violent Crime: $violentCrime <br>";
                 //Debug - Check DB connection
                 if ($link->connect_error) {
                     die("Connection failed: " . $link->connect_error);
                 }
                 echo "Connected successfully";
-
-                $sql = "SELECT * FROM tblNeighborhood";
-
-                $result = mysqli_query($link, $sql)
-                    or die('SQL syntax error: ' . mysqli_error($link));
-
-                $array = mysqli_fetch_array($result);
-
-                echo "<br>ID: ";
-                echo $array['neighborhoodID'];
                 ?>
+            <br>
+            <h1>Your Results:</h1>
+            <h3>Traffic: <?php echo $traffic?></h3>
+                <p>
+                    <?php
+                        fGetTraffic($link, $traffic);
+                    ?>
+                </p>
+            <h3>Housing: <?php echo $housing?></h3>
+                <p>
+                    <?php
+                        fGetHousing($link, $housing);
+                    ?>
+                </p>
+            <h3>Walkability: <?php echo $walk?></h3>
+                <p>
+                <?php
+                        fgetWalkability($link, $walk);
+                    ?>
+                </p>
+            <h3>Non-Violent Crime: <?php echo $nvCrime?></h3>
+                <p>
+                    <?php
+                        fgetNVCrime($link, $nvCrime);
+                    ?>
+                </p>
+            <h3>Rent Prices: <?php echo $rent?></h3>
+                <p>
+                    <?php
+                        fgetRent($link, $rent);
+                    ?>
+                </p>
+            <h3>Violent Crime: <?php echo $violentCrime?></h3>
+                <p>
+                    <?php
+                        fgetVCrime($link, $violentCrime);
+                    ?>
+                </p>
         </div>
     </div>
 </div>
--- include/connection.php
+++ include/connection.php
@@ -3,7 +3,7 @@
     //Pages that require database assess include it with include('connection.php');
     //Create a connection object
     //@ suppresses errors.
-    //parameters: mysqli_connect('my_server', 'my_user', 'my_password', 'my_db');
+    //parameters: mysqli_connect('my_server', 'my_user', 'my_password', 'my_
     function fConnectToDatabase() {
         $config = parse_ini_file('config.ini');
         $link = @mysqli_connect($config['host'], $config['username'], $config['password'], $config['dbname']);
@@ -15,7 +15,128 @@
         return $link;
     }
 
-    // database functions ************************************************
+    //data retrieval functions
+    //Returns traffic
+    function fGetTraffic ($link, $preference) {
+        $sql = "SELECT t.neighborhoodID, n.neighborhoodName, incidentCount
+                FROM tblTraffic t
+                INNER JOIN tblNeighborhood n
+                  ON n.neighborhoodID = t.neighborhoodID ";
+
+        if($preference == "high") $sql .= "ORDER BY worstToBestRank ASC LIMIT 5";
+        else $sql .= "ORDER BY worstToBestRank DESC LIMIT 5";
+
+        $result = mysqli_query($link, $sql)
+        or die('SQL Syntax Error ' . mysqli_error($link));
+
+        while($array = mysqli_fetch_array($result)) {
+            $neighborhoodID = $array['neighborhoodID'];
+            $neighborhoodName = $array['neighborhoodName'];
+            echo "<br>Neighborhood ID: $neighborhoodID - Neighborhood Name: $neighborhoodName";
+        }
+    }
+
+    //Returns housing
+    function fGetHousing ($link, $preference) {
+        $sql = "SELECT h.neighborhoodID, neighborhoodName, ZHVI, zhviRank 
+                FROM tblHousing h
+                INNER JOIN tblNeighborhood n
+		          ON n.neighborhoodID = h.neighborhoodID ";
+
+        if($preference == "high") $sql .= "ORDER BY zhviRank ASC LIMIT 5";
+        else $sql .= "ORDER BY zhviRank DESC LIMIT 5";
+
+        $result = mysqli_query($link, $sql)
+        or die('SQL Syntax Error ' . mysqli_error($link));
+
+        while($array = mysqli_fetch_array($result)) {
+            $neighborhoodID = $array['neighborhoodID'];
+            $neighborhoodName = $array['neighborhoodName'];
+            echo "<br>Neighborhood ID: $neighborhoodID - Neighborhood Name: $neighborhoodName";
+        }
+    }
+
+    //Returns walkscore
+    function fgetWalkability ($link, $preference) {
+        $sql = "SELECT ws.neighborhoodID, n.neighborhoodName, walkScore, restaurantCount 
+                FROM tblWalkScore ws
+	            INNER JOIN tblNeighborhood n
+		          ON n.neighborhoodID = ws.neighborhoodID ";
+
+        if($preference == "high") $sql .= "ORDER BY walkScore DESC LIMIT 5";
+        else $sql .= "ORDER BY walkScore ASC LIMIT 5";
+
+        $result = mysqli_query($link, $sql)
+        or die('SQL Syntax Error ' . mysqli_error($link));
+
+        while($array = mysqli_fetch_array($result)) {
+            $neighborhoodID = $array['neighborhoodID'];
+            $neighborhoodName = $array['neighborhoodName'];
+            echo "<br>Neighborhood ID: $neighborhoodID - Neighborhood Name: $neighborhoodName";
+        }
+    }
+
+    //Returns nonviolent crime
+    function fgetNVCrime ($link, $preference) {
+        $sql = "SELECT nv.neighborhoodID, n.neighborhoodName, incidentCount 
+                FROM tblNonViolentCrime nv
+                    INNER JOIN tblNeighborhood n
+                        ON n.neighborhoodID = nv.neighborhoodID ";
+
+        if($preference == "high") $sql .= "ORDER BY worstToBestRank ASC LIMIT 5";
+        else $sql .= "ORDER BY worstToBestRank DESC LIMIT 5";
+
+        $result = mysqli_query($link, $sql)
+        or die('SQL Syntax Error ' . mysqli_error($link));
+
+        while($array = mysqli_fetch_array($result)) {
+            $neighborhoodID = $array['neighborhoodID'];
+            $neighborhoodName = $array['neighborhoodName'];
+            echo "<br>Neighborhood ID: $neighborhoodID - Neighborhood Name: $neighborhoodName";
+        }
+    }
+
+    //Returns rent
+    function fgetRent ($link, $preference) {
+        $sql = "SELECT h.neighborhoodID, neighborhoodName, ZRI, zriRank 
+                FROM tblHousing h
+                INNER JOIN tblNeighborhood n
+                    ON n.neighborhoodID = h.neighborhoodID ";
+
+        if($preference == "high") $sql .= "ORDER BY zriRank DESC LIMIT 5";
+        else $sql .= "ORDER BY zriRank ASC LIMIT 5";
+
+        $result = mysqli_query($link, $sql)
+        or die('SQL Syntax Error ' . mysqli_error($link));
+
+        while($array = mysqli_fetch_array($result)) {
+            $neighborhoodID = $array['neighborhoodID'];
+            $neighborhoodName = $array['neighborhoodName'];
+            echo "<br>Neighborhood ID: $neighborhoodID - Neighborhood Name: $neighborhoodName";
+        }
+    }
+
+    //Returns violent crime
+    function fgetVCrime ($link, $preference) {
+        $sql = "SELECT v.neighborhoodID, n.neighborhoodName, incidentCount, worstToBestRank 
+                FROM tblViolentCrime v
+                INNER JOIN tblNeighborhood n
+                    ON n.neighborhoodID = v.neighborhoodID ";
+
+        if($preference == "high") $sql .= "ORDER BY worstToBestRank ASC LIMIT 5";
+        else $sql .= "ORDER BY worstToBestRank DESC LIMIT 5";
+
+        $result = mysqli_query($link, $sql)
+        or die('SQL Syntax Error ' . mysqli_error($link));
+
+        while($array = mysqli_fetch_array($result)) {
+            $neighborhoodID = $array['neighborhoodID'];
+            $neighborhoodName = $array['neighborhoodName'];
+            echo "<br>Neighborhood ID: $neighborhoodID - Neighborhood Name: $neighborhoodName";
+        }
+    }
+
+    // database utility functions ************************************************
     function fCleanString($link, $UserInput, $MaxLen) {
         //remove html tags
         $UserInput = strip_tags($UserInput);
