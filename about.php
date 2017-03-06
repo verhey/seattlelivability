7f35a1748e0394eb973e21a3472a7ac0fcf8799d
Dean Verhey <deanverhey@gmail.com>
2017-03-05 23:36:12 -0800

    add about page skeleton

---
+++ about.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!--Page basics-->
+    <title>Seattle Livability - About</title>
+    <meta name="description" content="Find your neighborhood in Seattle">
+    <?php include('include/supports.php') ?>
+</head>
+<body>
+
+<!--HEADER-->
+<?php include('include/header.php')?>
+
+<!--PAGE CONTENT-->
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12 text-center">
+            <h2>To Be Added Later:</h2>
+        </div>
+    </div>
+</div>
+
+
+<!--ABOUT CARDS-->
+<?php include('include/aboutcards.php')?>
+<br>
+
+<!--FOOTER-->
+<?php include('include/footer.php')?>
+</body>
+</html>
